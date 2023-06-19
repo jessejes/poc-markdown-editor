f55d3fa2dbe07d3da8b273d6106fcda4800cd698
jesse <92591280+jessejes@users.noreply.github.com>
2023-06-19 20:26:39 +0200

    Editable title

--- src/component/markdown-editor/index.tsx
+++ src/component/markdown-editor/index.tsx
@@ -1,5 +1,13 @@
+import './style.css'
+
 export default function MarkdownEditor() {
+
     return (
-        <h1>Markdown Editor</h1>
+        <div className="flex flex-col w-full">
+            <div id="title" className="text-2xl font-bold w-full outline-none break-words break-keep empty:text-neutral-300" placeholder="Untitled" contentEditable></div>
+            <div id="body" className="my-4">
+
+            </div>
+        </div>
     )
 }
