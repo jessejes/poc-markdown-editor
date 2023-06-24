4b6d16cc41674ccd7f5ccf6c09c6c0582a3722c6
jesse <92591280+jessejes@users.noreply.github.com>
2023-06-24 13:16:40 +0200

    Add title section and added padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import { Tab, TabList, TabPanel, TabPanels, Tabs, Textarea } from "@chakra-ui/react";
+import { Box, Editable, EditableInput, EditablePreview, Tab, TabList, TabPanel, TabPanels, Tabs, Textarea, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 import md from 'markdown-it';
 
 export default function App() {
 
+    const [title, setTitle] = useState<string>("")
     const [text, setText] = useState<string>("")
 
     return (
-        <Tabs h="full" w="100%">
-            <TabList>
-                <Tab>Markdown</Tab>
-                <Tab>Formatted</Tab>
-            </TabList>
-            <TabPanels>
-                <TabPanel>
-                    <Textarea value={text} onChange={event => setText(event.target.value)}  />
-                </TabPanel>
-                <TabPanel>
-                    <div className="prose w-full" dangerouslySetInnerHTML={{ __html: md().render(text) }} />
-                </TabPanel>
-            </TabPanels>
-        </Tabs>
+        <VStack p="12">
+            <Box display="flex" w="100%">
+                <Editable fontWeight="bold" defaultValue={title} placeholder="Untitled" >
+                    <EditablePreview />
+                    <EditableInput value={title} onChange={event => setTitle(event.target.value)} />
+                </Editable>
+            </Box>
+            <Tabs w="100%">
+                <TabList>
+                    <Tab>Markdown</Tab>
+                    <Tab>Formatted</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <Textarea h="xl" value={text} onChange={event => setText(event.target.value)}  />
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="prose w-full" dangerouslySetInnerHTML={{ __html: md().render(text) }} />
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </VStack>
+        
     )
 }
