e9a2cb02014cb74819872b01264b26a72cea4b86
jesse <92591280+jessejes@users.noreply.github.com>
2023-06-24 12:54:20 +0200

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import MarkdownEditor from "./component/markdown-editor";
+import { Tab, TabList, TabPanel, TabPanels, Tabs, Textarea } from "@chakra-ui/react";
+import { useState } from "react";
+import md from 'markdown-it';
 
 export default function App() {
+
+    const [text, setText] = useState<string>("")
+
     return (
-        <div className="w-full h-screen p-24">
-            <MarkdownEditor />
-        </div>
+        <Tabs h="full" w="100%">
+            <TabList>
+                <Tab>Markdown</Tab>
+                <Tab>Formatted</Tab>
+            </TabList>
+            <TabPanels>
+                <TabPanel>
+                    <Textarea value={text} onChange={event => setText(event.target.value)}  />
+                </TabPanel>
+                <TabPanel>
+                    <div className="prose w-full" dangerouslySetInnerHTML={{ __html: md().render(text) }} />
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
     )
 }
